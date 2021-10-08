6c0e3439e38a3c7e8cb98e9aa210f4d181df5685
Rakesh Birare <rakeshbirare9@gmail.com>
2021-10-08 18:27:20 +0530

    Cleaning Code
    Trying To Push The Same File With Minor Chenges To Understand GitHub

--- script/todo.js
+++ script/todo.js
@@ -1,28 +1,24 @@
 ul = document.getElementById('list');
 var li;
 
-
-
-
-
-
+// Add Event Listner To Add Todo Button
 var addButton = document.querySelector('#add');
 addButton.addEventListener('click',addItem);
 
+// Add Event Listner To Remove Todo Button
 var addButton = document.querySelector('#remove');
 addButton.addEventListener('click',removeItem);
 
 function addItem() {
     console.log("Item Added !")
 }
+
+// Remove Todo Functionality
 function removeItem() {
     li = ul.children
     for (let index = 0; index < li.length; index++) {
          while (li[index] && li[index].children[0].checked) {
              ul.removeChild(li[index])
-         }
-        
-        
-    }
-    
+         }   
+    }   
 }
